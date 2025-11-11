868f452c441a6a51960f96592a19019133a11982
dadc1007 <dadica1007@gmail.com>
2025-11-10 19:25:30 -0500

    Feature: GameInitializer modificado para integracion con Reactt

--- Assets/Scripts/Networks/GameInitializer.cs
+++ Assets/Scripts/Networks/GameInitializer.cs
@@ -10,28 +10,41 @@ namespace Networks
         public ArenaEntitySpawner arenaSpawner;
 
         [Header("DEBUG / TEST LOCAL")]
-        public string hubUrl = "http://localhost:5247/hubs/game";
-        public string sessionId = "b65bec75-ef58-4a5b-9973-292342ec87e6";
-        public string userId = "ba701be5-105c-4e92-93ec-92a6a6a68633";
+        public string hubUrl;
+        public string sessionId;
+        public string userId;
         public bool connectOnStart = true;
 
         public void SetConnectionData(string data)
         {
             string[] args = data.Split('|');
+            if (args.Length < 3)
+            {
+                Debug.LogError($"⚠️ Datos inválidos recibidos desde React: {data}");
+                return;
+            }
+
             string session = args[0];
             string user = args[1];
             string hub = args[2];
 
-            Debug.Log($"Datos recibidos -> Session:{session}, User:{user}, Hub:{hub}");
+            Debug.Log(
+                $"✅ Datos recibidos desde React -> Session:{session}, User:{user}, Hub:{hub}"
+            );
+
             gameClient.Connect(new Guid(session), new Guid(user), hub);
         }
 
         private void Start()
         {
+#if UNITY_EDITOR
             if (!connectOnStart)
                 return;
             Debug.Log($"[DEBUG] Conectando automáticamente a {hubUrl}");
             gameClient.Connect(new Guid(sessionId), new Guid(userId), hubUrl);
+#else
+            Debug.Log("Esperando parámetros desde React...");
+#endif
         }
     }
 }
